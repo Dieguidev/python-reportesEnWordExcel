e22122da11233996597be5196ba1abffd4823a79
Dieguidev <diegogaraycullas@gmail.com>
2024-01-21 15:13:17 -0500

    filtrando datos

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ print(NOTAS_ALUMNOS_PATH)
 
 
 def main():
-    # Lee el archivo Excel y carga los datos en un DataFrame usando pandas
+    #* Lee el archivo Excel y carga los datos en un DataFrame usando pandas
     excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
 
-    # Itera sobre cada fila del DataFrame
+    #* Itera sobre cada fila del DataFrame
     for index, row in excel_df.iterrows():
-        # Imprime el índice de la fila, el valor de la columna 'NOMBRE' y el valor de la columna 'NOTA T1'
+        #* Imprime el índice de la fila, el valor de la columna 'NOMBRE' y el valor de la columna 'NOTA T1'
         print(index, row['NOMBRE'], row['NOTA T1'])
+        
+    
+    asig_list = list(excel_df['ASIGNATURA'].drop_duplicates())
+    print(asig_list)
     
 
 if __name__=='__main__':
