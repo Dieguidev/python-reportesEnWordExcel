4eb22bd3583fe51d3e70e0da73fe62ca6acdff15
Dieguidev <diegogaraycullas@gmail.com>
2024-01-21 14:00:02 -0500

    leido el archivo y mostrado por consola

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ print(NOTAS_ALUMNOS_PATH)
 
 
 def main():
-    excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH)
+    excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
+    for index, row in excel_df.iterrows():
+        print(index, row['NOMBRE'], row['NOTA T1'])
+    
 
 if __name__=='__main__':
     main()
