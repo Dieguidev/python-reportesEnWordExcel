b52a5977406714de4f926bc4af59ba8642fe81d7
Dieguidev <diegogaraycullas@gmail.com>
2024-01-21 14:02:21 -0500

    comentando el codigo

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ print(NOTAS_ALUMNOS_PATH)
 
 
 def main():
+    # Lee el archivo Excel y carga los datos en un DataFrame usando pandas
     excel_df = pd.read_excel(NOTAS_ALUMNOS_PATH, sheet_name='Notas')
+
+    # Itera sobre cada fila del DataFrame
     for index, row in excel_df.iterrows():
+        # Imprime el índice de la fila, el valor de la columna 'NOMBRE' y el valor de la columna 'NOTA T1'
         print(index, row['NOMBRE'], row['NOTA T1'])
     
 
